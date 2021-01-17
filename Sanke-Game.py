62004cc1c869f16a4fcc8624264b20cea8db3f8a
raghu <raghumina11@gmail.com>
2021-01-17 21:25:28 +0530

    Update Sanke-Game.py

--- Sanke-Game.py
+++ Sanke-Game.py
@@ -1,7 +1,7 @@
 # SNAKE GAME
 # THIS SNAKE GAME WAS MADE WITH THE HELP OF PYGAME MODULE
 # PYTHON PYGAME MODULE IS USE TO CREATE GAMES
-
+import sys
 import pygame
 
 pygame.init()
@@ -10,4 +10,8 @@ screen = pygame.display.set_mode((600, 600))  # this is length and breadth of th
 
 while True:
     # Here we will draw all our elements such as dispaly image, snake, fruits etc
+    for event in pygame.event.get():
+        event.type == pygame.QUIT
+        pygame.quit()
+        sys.exit
     pygame.display.update()
