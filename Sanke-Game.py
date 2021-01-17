41f069e312dd7c02c797cbe7042350e337b3256d
raghu <raghumina11@gmail.com>
2021-01-17 21:23:17 +0530

    Update Sanke-Game.py

--- Sanke-Game.py
+++ Sanke-Game.py
@@ -3,4 +3,11 @@
 # PYTHON PYGAME MODULE IS USE TO CREATE GAMES
 
 import pygame
+
 pygame.init()
+
+screen = pygame.display.set_mode((600, 600))  # this is length and breadth of the game display
+
+while True:
+    # Here we will draw all our elements such as dispaly image, snake, fruits etc
+    pygame.display.update()
