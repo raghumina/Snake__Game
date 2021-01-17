a41db29e4878fa4f255a66bd48c9d6189037d8cb
raghu <raghumina11@gmail.com>
2021-01-17 22:14:16 +0530

    Update Sanke-Game.py

--- Sanke-Game.py
+++ Sanke-Game.py
@@ -6,9 +6,12 @@ import pygame
 
 pygame.init()
 
+cell_size = 20
+cell_number = 40
+
 screen = pygame.display.set_mode((400, 500))  # this is length and breadth of the game display
 clock = pygame.time.Clock()  # to make game more consistent time wise
-test_surface = pygame.Surface((100, 200))
+test_surface = pygame.Surface((10, 20))
 test_surface.fill(pygame.Color("black"))
 test_rect = test_surface.get_rect(topright = (200,250))
 
@@ -22,6 +25,6 @@ while True:
   #  pygame.draw.rect(screen, pygame.Color("red"), test_rect)
     test_rect.right +=1
     screen.fill(pygame.Color("Green"))
-    screen.blit(test_surface, (200, 250))  # blit stands for block image transfer
+    screen.blit(test_surface, test_rect)  # blit stands for block image transfer
     pygame.display.update()
     clock.tick(60)  # 60 frames per second
