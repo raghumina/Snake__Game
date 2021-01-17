289e5e1bebe95d88e4665e91af0936dee54935b1
raghu <raghumina11@gmail.com>
2021-01-17 21:46:43 +0530

    Update Sanke-Game.py

--- Sanke-Game.py
+++ Sanke-Game.py
@@ -6,10 +6,9 @@ import pygame
 
 pygame.init()
 
-
 screen = pygame.display.set_mode((600, 600))  # this is length and breadth of the game display
 clock = pygame.time.Clock()  # to make game more consistent time wise
-test_surface = pygame.Surface((100,200))
+test_surface = pygame.Surface((100, 200))
 
 while True:
     # Here we will draw all our elements such as dispaly image, snake, fruits etc
@@ -17,6 +16,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit
-    screen.blit(test_surface(200,200))   # blit stands for block image transfer 
+
+    screen.fill(pygame.Color("Green"))
+    screen.blit(test_surface, (200, 250))  # blit stands for block image transfer
     pygame.display.update()
-    clock.tick(60)     # 60 frames per second
+    clock.tick(60)  # 60 frames per second
